a2dfa14a2cb2f1b355abe8f52b8aeadba290371a
Almir Martins Junior <almirnal@gmail.com>
2022-07-19 12:08:04 -0300

    introdução a variáveis

---
+++ 13-Javascript Básico/04-Variaveis-var,let,const/script.js
@@ -0,0 +1,30 @@
+//como declarar variaveis
+// var, let, const
+
+
+// string
+var nome ='Almir'
+
+// number
+var idade = 41
+
+console.log(nome, idade)
+
+// O metodo var não esta sendo mais utilizado.
+// as variaveis não pode ter espaço, utilizar underline ou kamel case.
+// não pode iniciar com numeros.
+
+
+
+// o let pode alterar o valor deles
+let nome ='Almir'
+ nome = 'Martins' // subscrever
+
+
+console.log(nome)
+
+// o const não pode ser alterado o valor
+const nome ='Almir'
+ 
+
+console.log(nome)
