1033b0fc584fc8d1f2354b504e6c1658e91af7d2
Almir Martins Junior <almirnal@gmail.com>
2022-07-20 08:49:54 -0300

    Operadores Aritméticos e de Atribuição

---
+++ 13-Javascript Básico/06-operadores aritiméticos/script.js
@@ -0,0 +1,70 @@
+/*
+OPERADORES ARITIMÉTICOS
+adição: +
+subtração: -
+multiplicação: *
+divisão: /
+incremento: ++
+decremento: --
+
+OPERADORES DE ATRIBUIÇÃO
++=  -=  *=  /=
+
+NaN = Not a Number
+
+*/
+
+
+// adição
+let a = 10
+let b = 5
+let c = a + b 
+console.log(c)
+
+//subtração
+let d = 10
+let e = 5
+let f = d - e 
+console.log(f)
+
+//multiplicação
+let g = 10
+let h = 5
+let i = g * h 
+console.log(i)
+
+//divisão
+let j = 10
+let k = 5
+let l = j / k
+console.log(l)
+
+//incremento
+let m = 10
+m++
+console.log(m)
+
+//decremento
+let n = 10
+n--
+console.log(n)
+
+//atribuição +
+let o = 10
+o += 5   // o = o + 5
+console.log(o)
+
+//atribuição -
+let p = 10
+p -= 5   // p = p - 5
+console.log(p)
+
+//atribuição *
+let q = 10
+q *= 5   // q = q * 5
+console.log(q)
+
+//atribuição /
+let r = 10
+r /= 5   // r = r / 5
+console.log(r)
