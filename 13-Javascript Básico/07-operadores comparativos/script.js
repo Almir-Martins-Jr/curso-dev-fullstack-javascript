3ad0d68e071d2b7aec900dae77d90c6d9ea17e86
Almir Martins Junior <almirnal@gmail.com>
2022-07-20 09:26:21 -0300

    Operadores Comparativos

---
+++ 13-Javascript Básico/07-operadores comparativos/script.js
@@ -0,0 +1,52 @@
+/*
+
+==  igualdade
+
+=== igualdade de valores e tipos
+
+!=  diferença de valores
+
+!== diferença de valores e tipos
+
+>  maior que 
+
+<  menor que
+
+>= maior ou igual
+
+<= menor ou igual
+
+*/
+
+
+let a = 10
+let b = 10
+console.log(a == b)
+
+let c = 10
+let d = 10
+console.log(c === d)
+
+let e = 10
+let f = 10
+console.log(e != f)
+
+let g = 10
+let h = 10
+console.log(g !== h)
+
+let i = 10
+let j = 10
+console.log(i > j)
+
+let k = 10
+let l = 10
+console.log(k < l)
+
+let m = 10
+let n = 10
+console.log(m >= n)
+
+let o = 10
+let p = 10
+console.log(o >= p)
