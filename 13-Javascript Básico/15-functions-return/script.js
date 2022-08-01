3627f87c83bc478f5d2e4b691d90c6f7219e493c
Almir Martins <almirnal@gmail.com>
2022-08-01 18:12:04 -0300

    Functions - Return

---
+++ 13-Javascript Básico/15-functions-return/script.js
@@ -0,0 +1,20 @@
+// function soma(num1, num2) {
+//   return num1 + num2
+// }
+
+// let result = soma(20, 55)
+
+// console.log(result)
+
+// document.write(result)
+
+// o valor do return precisa ser atribuido a uma variavel
+
+function milhasEmKm(milhas) {
+  let km = milhas *1.6
+
+  return km
+}
+
+let resultado = milhasEmKm(150)
+document.write(resultado)
