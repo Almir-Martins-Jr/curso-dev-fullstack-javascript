af408e832983abe3a8d51b1b93cef85bd8883206
Almir Martins <almirnal@gmail.com>
2022-08-19 10:09:04 -0300

    adicionando mais comentários

--- 14-Javascript Intermediario/03-seletores/script.js
+++ 14-Javascript Intermediario/03-seletores/script.js
@@ -1,14 +1,14 @@
-// essa função tem o return dentro, e retorna um objeto com varias opções. uma dessas opçoes é o innerHTML.
+// essa função tem o return dentro, e retorna um objeto com varias opções. uma dessas opçoes é o innerHTML, que altera o conteúdo no html.
 
-//SELETOR PO ID
-document.querySelector('#titulo').innerHTML='<i>obelisco</i>'
-
-
+//SELETOR POR ID
+document.querySelector('#titulo').innerHTML = '<i>obelisco</i>'
 
 
-//SELETOR PO TAG
+
+
+//SELETOR POR TAG
 // o querySelector seleciona somente uma ocorrência
-document.querySelector('a').innerHTML='Cartas'
+document.querySelector('a').innerHTML = 'Cartas'
 
 
 
@@ -17,8 +17,8 @@ document.querySelector('a').innerHTML='Cartas'
 // o querySelectorall seleciona todas ocorrências
 let ancoras = document.querySelectorAll('a')
 
-ancoras.forEach(function(elemento){ // função anonima /callback
-  elemento.innerHTML ='Cartas'
+ancoras.forEach(function (elemento) { // função anonima /callback
+  elemento.innerHTML = 'Cartas'
 })
 // for each percorre o array retornando seu conteúdo
 
@@ -26,21 +26,17 @@ ancoras.forEach(function(elemento){ // função anonima /callback
 
 
 // SELECIONANDO MAIS DE UM ITEM POR CLASSES. EXEMPLOS:
-let boxes = document.querySelectorAll ('.box')
+let boxes = document.querySelectorAll('.box')
 
-boxes.forEach(function(box, index){
-box.innerHTML = 'box' + (index + 1)
+boxes.forEach(function (box, index) {
+  box.innerHTML = 'box' + (index + 1)
 
 
-// let count = 0
-// boxes.forEach(function(box){
-//  count++
- 
-//   box.innerHTML = 'box' + count
+
+  // let boxes = document.querySelectorAll ('.box')
+  // let count = 0
+  // boxes.forEach(function(box){
+  //  count++
+
+  //   box.innerHTML = 'box' + count
 })
-
-
-
-
-
-
